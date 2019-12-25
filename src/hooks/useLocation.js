b01e7acc38d8b4b00826f18a059b5c4408435639
Roger Generoso Masós <roger21gm@gmail.com>
2019-12-25 20:16:39 +0100

    Extracted location tracking to a reusable hook.

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,25 @@
+import {useState, useEffect, useContext} from 'react';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+
+export default (callback) => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10,
+            }, callback);
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
+    return [err];
+};
